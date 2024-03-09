942adfabd29955b657981af49ff39ea2d0d23aca
max <68344959+codewmax@users.noreply.github.com>
2024-03-09 23:57:00 +0100

    Update data file path in parse-readme.js

--- docs/parse-readme.js
+++ docs/parse-readme.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const filePath = path.join(__dirname, '..', 'README.md');
-const dataFilePath = path.join(__dirname, 'docs', 'data.json');
+const dataFilePath = path.join(__dirname, 'data.json');
 
 fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
